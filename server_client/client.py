39855482dd4986f7f1e2ee08ca9e1be4823ce7de
mdkmk <mkirby.engineering@gmail.com>
2024-03-21 13:18:30 +0200

    Initial Code

---
+++ server_client/client.py
@@ -0,0 +1,63 @@
+import socket
+import select
+import errno
+import sys
+import threading
+
+class ChatClient:
+    HEADER_LENGTH = 10
+
+    def __init__(self, ip, port, username):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.ip, self.port))
+        self.client_socket.setblocking(False)
+        username_encoded = self.username.encode("utf-8")
+        username_header = f"{len(username_encoded):<{self.HEADER_LENGTH}}".encode("utf-8")
+        self.client_socket.send(username_header + username_encoded)
+
+    def send_message(self):
+        while True:
+            message = input(f"{self.username} > ")
+            if message:
+                message_encoded = message.encode("utf-8")
+                message_header = f"{len(message_encoded):<{self.HEADER_LENGTH}}".encode("utf-8")
+                self.client_socket.send(message_header + message_encoded)
+
+    def receive_message(self):
+        while True:
+            try:
+                while True:
+                    username_header = self.client_socket.recv(self.HEADER_LENGTH)
+                    if not len(username_header):
+                        print("Connection closed by the server")
+                        sys.exit()
+                    username_length = int(username_header.decode("utf-8").strip())
+                    username = self.client_socket.recv(username_length).decode("utf-8")
+                    message_header = self.client_socket.recv(self.HEADER_LENGTH)
+                    message_length = int(message_header.decode("utf-8").strip())
+                    message = self.client_socket.recv(message_length).decode("utf-8")
+                    print(f"{username} > {message}")
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print("Reading error", str(e))
+                    sys.exit()
+                continue
+            except Exception as e:
+                print("General error", str(e))
+                sys.exit()
+
+    def run(self):
+        send_thread = threading.Thread(target=self.send_message)
+        receive_thread = threading.Thread(target=self.receive_message)
+        send_thread.start()
+        receive_thread.start()
+        send_thread.join()
+        receive_thread.join()
+
+if __name__ == "__main__":
+    username = input("Username: ")
+    client = ChatClient("127.0.0.1", 1234, username)
+    client.run()
---
+++ server_client/server.py
@@ -0,0 +1,58 @@
+import socket
+import select
+
+class ChatServer:
+    HEADER_LENGTH = 10
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.ip, self.port))
+        self.server_socket.listen()
+        self.sockets_list = [self.server_socket]
+        self.clients = {}
+
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(self.HEADER_LENGTH)
+            if not len(message_header):
+                return False
+            message_length = int(message_header.decode("utf-8").strip())
+            return {"header": message_header, "data": client_socket.recv(message_length)}
+        except:
+            return False
+
+    def run(self):
+        while True:
+            read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
+            for notified_socket in read_sockets:
+                if notified_socket == self.server_socket:
+                    client_socket, client_address = self.server_socket.accept()
+                    user = self.receive_message(client_socket)
+                    if user is False:
+                        continue
+                    self.sockets_list.append(client_socket)
+                    self.clients[client_socket] = user
+                    print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+                else:
+                    message = self.receive_message(notified_socket)
+                    if message is False:
+                        print(f"Closed connection from {self.clients[notified_socket]['data'].decode('utf-8')}")
+                        self.sockets_list.remove(notified_socket)
+                        del self.clients[notified_socket]
+                        continue
+                    user = self.clients[notified_socket]
+                    print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+                    for client_socket in self.clients:
+                        if client_socket != notified_socket:
+                            client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+            for notified_socket in exception_sockets:
+                self.sockets_list.remove(notified_socket)
+                del self.clients[notified_socket]
+
+if __name__ == "__main__":
+    server = ChatServer("127.0.0.1", 1234)
+    server.run()
